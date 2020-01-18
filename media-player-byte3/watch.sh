52dd3a57053b6e185a10fc97a670b79104a3b5d7
Zachary Vance <za3k@za3k.com>
2020-01-18 00:41:17 -0800

    remove media-player-rclone

--- media-player-byte3/watch.sh
+++ media-player-byte3/watch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rclone -c /var/local/media-player/rclone.conf mount germinate: /media/germinate &
 sleep 1
-/var/local/media-player/bin/noice /media/germinate
+/var/local/media-player/noice /media/germinate
 kill %1
